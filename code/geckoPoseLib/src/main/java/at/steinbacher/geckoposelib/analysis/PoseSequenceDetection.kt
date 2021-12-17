a628cfab00b59a400725a11bf5d394cbc14737fd
Georg Steinbacher <georgsteinbacher@gmx.net>
2021-12-17 15:00:35 +0100

    added PoseSequenceDetection

---
+++ code/geckoPoseLib/src/main/java/at/steinbacher/geckoposelib/analysis/PoseSequenceDetection.kt
@@ -0,0 +1,30 @@
+package at.steinbacher.geckoposelib.analysis
+
+import at.steinbacher.geckoposelib.data.NormalizedPoseVideo
+import kotlin.math.abs
+
+data class SequenceAngleMark(
+    val angleTag: String,
+    val targetValue: Double
+)
+
+class PoseSequenceDetection(
+    val poseVideo: NormalizedPoseVideo,
+) {
+
+    data class SequenceScore(val timeStamp: Long, val distance: Double)
+
+    fun find(sequence: List<SequenceAngleMark>): Long {
+        val sequenceScores = poseVideo.normalizedPoseFrames.map { normalizedPoseFrame ->
+            SequenceScore(normalizedPoseFrame.timestamp, if(normalizedPoseFrame.normalizedPose != null) {
+                sequence.fold(0.0) { acc, sequenceAngleMark ->
+                    acc + abs(normalizedPoseFrame.normalizedPose.getAngle(sequenceAngleMark.angleTag).value - sequenceAngleMark.targetValue)
+                }
+            } else {
+               Double.MAX_VALUE
+            })
+        }
+
+        return sequenceScores.minByOrNull { it.distance }?.timeStamp ?: error("Unable to find smallest distance in list! Is it empty?")
+    }
+}

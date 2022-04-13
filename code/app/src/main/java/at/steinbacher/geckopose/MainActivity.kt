6d70d1dd739be61f6fc4d77d2d0b0c95ff85c79f
Georg Steinbacher <g.steinbacher@reply.de>
2022-04-13 09:15:36 +0200

    working setup

--- code/app/src/main/java/at/steinbacher/geckopose/MainActivity.kt
+++ code/app/src/main/java/at/steinbacher/geckopose/MainActivity.kt
@@ -1,11 +1,19 @@
 package at.steinbacher.geckopose
 
 import android.os.Bundle
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material3.MaterialTheme
+import at.steinbacher.geckoposelib.component.GeckoPoseView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        setContent {
+            MaterialTheme {
+                GeckoPoseView()
+            }
+        }
     }
 }
---
+++ code/geckoPoseLib/src/main/java/at/steinbacher/geckoposelib/component/GeckoPoseView.kt
@@ -0,0 +1,9 @@
+package at.steinbacher.geckoposelib.component
+
+import androidx.compose.runtime.Composable
+
+
+@Composable
+fun GeckoPoseView() {
+
+}

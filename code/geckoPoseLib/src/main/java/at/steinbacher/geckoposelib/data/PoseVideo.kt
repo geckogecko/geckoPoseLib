3ae82e3544c2901a0024edfbd19bb7f39da536da
Georg Steinbacher <g.steinbacher@reply.de>
2022-05-03 15:18:19 +0200

    added getSmoothPoses

--- code/geckoPoseLib/src/main/java/at/steinbacher/geckoposelib/data/PoseVideo.kt
+++ code/geckoPoseLib/src/main/java/at/steinbacher/geckoposelib/data/PoseVideo.kt
@@ -1,5 +1,6 @@
 package at.steinbacher.geckoposelib.data
 
+import at.steinbacher.geckoposelib.util.AngleUtil
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
 import java.lang.Exception
@@ -10,7 +11,59 @@ data class PoseVideo(
     val uri: String,
     val poses: List<PoseFrame>,
     val normalizedPoses: List<NormalizedPoseFrame>
-)
+) {
+    suspend fun getSmoothPoses(
+        periods: Int,
+    ): List<NormalizedPoseFrame> {
+        return normalizedPoses.windowed(size = periods, step = 1, partialWindows = true)
+            .map {
+                //smoothPoints
+                val smoothPoints = it.first().normalizedPose.configuration.pointConfigurations.map { pointConfiguration ->
+                    val averagePosition = it.map { normalizedPoseFrame ->
+                        normalizedPoseFrame.normalizedPose.getPoint(pointConfiguration.type).position
+                    }.average()
+
+                    Point(
+                        position = averagePosition,
+                        pointConfiguration = pointConfiguration,
+                        inFrameLikelihood = it.first().normalizedPose.getPoint(pointConfiguration.type).inFrameLikelihood
+                    )
+                }
+
+                //newAngles
+                val angles = it.first().normalizedPose.configuration.angleConfigurations.map { angleConfiguration ->
+                    Angle(
+                        tag = angleConfiguration.tag,
+                        startPointType = angleConfiguration.startPointType,
+                        middlePointType = angleConfiguration.middlePointType,
+                        endPointType = angleConfiguration.endPointType,
+                        value = AngleUtil.getAngle(
+                            smoothPoints.find { sp -> sp.pointConfiguration.type == angleConfiguration.startPointType }!!,
+                            smoothPoints.find { sp -> sp.pointConfiguration.type == angleConfiguration.middlePointType }!!,
+                            smoothPoints.find { sp -> sp.pointConfiguration.type == angleConfiguration.endPointType }!!
+                        )
+                    )
+                }
+                NormalizedPoseFrame(
+                    frameNr = it.first().frameNr,
+                    normalizedPose = NormalizedGeckoPose(
+                        points = smoothPoints,
+                        angles = angles,
+                        configuration = it.first().normalizedPose.configuration
+                    )
+                )
+            }
+    }
+
+    private fun List<PointF>.average(): PointF {
+        var x = 0.0
+        var y = 0.0
+        return PointF(
+            x = (this.sumOf { it.x.toDouble() } / this.size).toFloat(),
+            y = (this.sumOf { it.x.toDouble() } / this.size).toFloat()
+        )
+    }
+}
 
 @Serializable
 data class PoseFrame(

754d91400ac97a112672c5ba24fb8ceed5ae0f3a
Georg Steinbacher <g.steinbacher@reply.de>
2022-05-29 19:00:19 +0200

    added missing tag for smooth frames

--- code/geckoPoseLib/src/main/java/at/steinbacher/geckoposelib/data/PoseVideo.kt
+++ code/geckoPoseLib/src/main/java/at/steinbacher/geckoposelib/data/PoseVideo.kt
@@ -47,6 +47,7 @@ data class PoseVideo(
                     )
                 }
                 NormalizedPoseFrame(
+                    tag = it.first().tag,
                     frameNr = it.first().frameNr,
                     normalizedPose = NormalizedGeckoPose(
                         points = smoothPoints,
@@ -84,51 +85,4 @@ data class NormalizedPoseFrame(
     val normalizedPose: NormalizedGeckoPose,
     val tag: String? = null,
 )
-
-
-/*
-@Serializable
-data class NormalizedPoseVideo(val uri: String, val timestampSteps: Int, val normalizedPoseFrames: List<NormalizedPoseFrame>) {
-    fun getAngles(angleTag: String): List<Angle> = normalizedPoseFrames.mapNotNull { it.normalizedPose?.getAngle(angleTag) }
-
-    fun getByTimestamp(timestamp: Long): NormalizedPoseFrame
-        = normalizedPoseFrames.find { it.timestamp == timestamp } ?: error("No frame with $timestamp found!")
-
-    fun getClosestFrame(timestamp: Long): NormalizedPoseFrame = normalizedPoseFrames.minByOrNull {
-        abs(timestamp - it.timestamp)
-    } ?: error("PoseVideo has no Frames!")
-
-    private fun List<Float>.scale(sampleSize: Int): List<Float> {
-        if(this.size < sampleSize) {
-            throw Exception("Sample size > line size! $sampleSize - ${this.size}")
-        }
-
-        val sampleSteps = this.size / sampleSize.toFloat()
-
-        var currentScaledPointsIndex = 0
-        val scaledPoints = ArrayList<Float>()
-        for(i in 0 until sampleSize) {
-            scaledPoints.add(0f)
-        }
-
-        val currentSampleTargets = ArrayList<Float>()
-        var currentStepLimit = sampleSteps
-        for(i in this.indices) {
-            if(i < currentStepLimit) {
-                currentSampleTargets.add(this[i])
-            } else {
-                scaledPoints[currentScaledPointsIndex] = currentSampleTargets.average().toFloat()
-                currentScaledPointsIndex++
-                currentStepLimit += sampleSteps
-                currentSampleTargets.clear()
-                currentSampleTargets.add(this[i])
-            }
-        }
-        scaledPoints[sampleSize-1] = currentSampleTargets.average().toFloat()
-
-        return scaledPoints.toList()
-    }
-}
-
- */
 
